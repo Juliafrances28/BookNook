547fbe7111a5c96858f4d077f53d0e1df003b5e0
Julia Frances <julia.horan28@gmail.com>
2022-05-14 22:11:34 -0400

    made some changes to my tabs by turning s ome of the nested react components into html and adding the proper styling

--- src/pages/Books.js
+++ src/pages/Books.js
@@ -1,115 +1,168 @@
 import React from "react";
 import Container from "../components/Container/index";
-import Row from "../components/Row/index";
+// import Row from "../components/Row/index";
 // import Image from "../components/image/index";
 import Button from "../components/Button/index";
 import Header from "../components/Header/index";
 import Card from "../components/Card/index";
 import "./style.css";
+import Wrapper from "../components/Wrapper/index";
 
-function Books() {
-  return (
-    <div>
-      <Container>
-        <Card>
-          <Row>
-            <div>
-              <img
-                src="https://images-na.ssl-images-amazon.com/images/I/81dvRHsl2KL.jpg"
-                className="book"
-                alt="book"
-              />
+class Books extends React.Component {
+  render() {
+    return (
+      <div>
+        <Container>
+          <Card>
+            <div className="row">
+              <div className="col">
+                <div>
+                  <img
+                    src="https://images-na.ssl-images-amazon.com/images/I/81dvRHsl2KL.jpg"
+                    className="book"
+                    alt="book"
+                  />
+                </div>
+                <Header></Header>
+                <Button></Button>
+              </div>
             </div>
-            <Header></Header>
-            <Button></Button>
-          </Row>
-        </Card>
+          </Card>
 
-        <Card>
-          <Row>
-            <div>
-              <img
-                src="https://images-na.ssl-images-amazon.com/images/I/81dvRHsl2KL.jpg"
-                className="book"
-                alt="book"
-              />
+          <Card>
+            <div className="row">
+              <div className="col">
+                <div>
+                  <img
+                    src="https://images-na.ssl-images-amazon.com/images/I/81dvRHsl2KL.jpg"
+                    className="book"
+                    alt="book"
+                  />
+                </div>
+                <Header></Header>
+                <Button></Button>
+              </div>
             </div>
-            <Header></Header>
-            <Button></Button>
-          </Row>
-        </Card>
+          </Card>
 
-        <Card>
-          <Row>
-            <div>
-              <img
-                src="https://images-na.ssl-images-amazon.com/images/I/81dvRHsl2KL.jpg"
-                className="book"
-                alt="book"
-              />
+          <Card>
+            <div className="row">
+              <div className="col">
+                <div>
+                  <img
+                    src="https://images-na.ssl-images-amazon.com/images/I/81dvRHsl2KL.jpg"
+                    className="book"
+                    alt="book"
+                  />
+                </div>
+                <Header></Header>
+                <Button></Button>
+              </div>
             </div>
-            <Header></Header>
-            <Button></Button>
-          </Row>
-        </Card>
+          </Card>
+        </Container>
 
-        <Card>
-          <Row>
-            <div>
-              <img
-                src="https://images-na.ssl-images-amazon.com/images/I/81dvRHsl2KL.jpg"
-                className="book"
-                alt="book"
-              />
+        <Container>
+          <Card>
+            <div className="row">
+              <div className="col">
+                <div>
+                  <img
+                    src="https://images-na.ssl-images-amazon.com/images/I/81dvRHsl2KL.jpg"
+                    className="book"
+                    alt="book"
+                  />
+                </div>
+                <Header></Header>
+                <Button></Button>
+              </div>
             </div>
-            <Header></Header>
-            <Button> </Button>
-          </Row>
-        </Card>
+          </Card>
 
-        <Card>
-          <Row>
-            <div>
-              <img
-                src="https://images-na.ssl-images-amazon.com/images/I/81dvRHsl2KL.jpg"
-                className="book"
-                alt="book"
-              />
+          <Card>
+            <div className="row">
+              <div className="col">
+                <div>
+                  <img
+                    src="https://images-na.ssl-images-amazon.com/images/I/81dvRHsl2KL.jpg"
+                    className="book"
+                    alt="book"
+                  />
+                </div>
+                <Header></Header>
+                <Button></Button>
+              </div>
             </div>
-            <Header></Header>
-            <Button></Button>
-          </Row>
-        </Card>
+          </Card>
 
-        <Card>
-          <Row>
-            <div>
-              <img
-                src="https://images-na.ssl-images-amazon.com/images/I/81dvRHsl2KL.jpg"
-                className="book"
-                alt="book"
-              />
+          <Card>
+            <div className="row">
+              <div className="col">
+                <div>
+                  <img
+                    src="https://images-na.ssl-images-amazon.com/images/I/81dvRHsl2KL.jpg"
+                    className="book"
+                    alt="book"
+                  />
+                </div>
+                <Header></Header>
+                <Button></Button>
+              </div>
             </div>
-            <Header></Header>
-            <Button></Button>
-          </Row>
-        </Card>
+          </Card>
+        </Container>
+        <Container>
+          <Card>
+            <div className="row">
+              <div className="col">
+                <div>
+                  <img
+                    src="https://images-na.ssl-images-amazon.com/images/I/81dvRHsl2KL.jpg"
+                    className="book"
+                    alt="book"
+                  />
+                </div>
+                <Header></Header>
+                <Button></Button>
+              </div>
+            </div>
+          </Card>
 
-        <Card>
-          <Row>
-            <div>
-              <img
-                src="https://images-na.ssl-images-amazon.com/images/I/81dvRHsl2KL.jpg"
-                className="book"
-                alt="book"
-              />
+          <Card>
+            <div className="row">
+              <div className="col">
+                <div>
+                  <img
+                    src="https://images-na.ssl-images-amazon.com/images/I/81dvRHsl2KL.jpg"
+                    className="book"
+                    alt="book"
+                  />
+                </div>
+                <Header></Header>
+                <Button></Button>
+              </div>
             </div>
-            <Header></Header>
-            <Button></Button>
-          </Row>
-        </Card>
-      </Container>
-    </div>
-  );
+          </Card>
+
+          <Card>
+            <div className="row">
+              <div className="col">
+                <div>
+                  <img
+                    src="https://images-na.ssl-images-amazon.com/images/I/81dvRHsl2KL.jpg"
+                    className="book"
+                    alt="book"
+                  />
+                </div>
+                <Header></Header>
+                <Button></Button>
+              </div>
+            </div>
+          </Card>
+        </Container>
+      </div>
+    );
+  }
 }
+
 export default Books;
